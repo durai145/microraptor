15e9bd03a928637c9159036a738e8f1c013ab519
Duraimurugan Govindaraj <durai145@gmail.com>
2020-06-11 10:40:58 -0400

    MICRORAPTOR-001 fixed api

--- js/angular/services/loginService.js
+++ js/angular/services/loginService.js
@@ -2,7 +2,7 @@ define([],function(){
 
 return ['$resource',function  ($resource) {
 	// body...
-	return $resource('/pillar/user/v2/:task', null,
+	return $resource('/api/pillar/user/v2/:task', null,
     {
         doLogin  : { method: 'POST', params: {"task" : "login"}} ,
         register  : { method: 'POST', params: {"task" : "register"}} 
